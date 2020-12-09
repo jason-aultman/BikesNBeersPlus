f09ddeea277314b0eddeb40aa13e1b7a74d54b41
Jason Aultman <jason.aultman@gmail.com>
2020-12-09 10:05:19 -0500

    database identity add

---
+++ BikesNBeersMVC/Context/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikesNBeersMVC.Context
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+   
+
+    public class DesignTimeDbContextFactory:IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseSqlServer("Server=tcp:bikesnbeers-sqlserver.database.windows.net,1433;Initial Catalog=BikesNBeersDb;Persist Security Info=False;User ID=bikesnbeersadmin;Password=bikesnbeers!123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
--- BikesNBeersMVC/Startup.cs
+++ BikesNBeersMVC/Startup.cs
@@ -8,12 +8,13 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using BikesNBeersMVC.Data;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BikesNBeersMVC.Services;
 using BikesNBeersMVC.Services.Interfaces;
+using BikesNBeersMVC.Context;
 
 namespace BikesNBeersMVC
 {
