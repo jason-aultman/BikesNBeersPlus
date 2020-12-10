257a37bc2229d86998d9190042d909561fc4abbe
Jennifer <jebalas7@gmail.com>
2020-12-09 22:16:34 -0500

    Scaffold Trip Controller

---
+++ BikesNBeersMVC/Controllers/TripController.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BikesNBeersMVC.Context;
+using BikesNBeersMVC.Models;
+
+namespace BikesNBeersMVC.Controllers
+{
+    public class TripController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TripController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Trip
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Trips.Include(t => t.BikerInfo);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Trip/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trip = await _context.Trips
+                .Include(t => t.BikerInfo)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return View(trip);
+        }
+
+        // GET: Trip/Create
+        public IActionResult Create()
+        {
+            ViewData["BikerInfoId"] = new SelectList(_context.BikerInfos, "Id", "Id");
+            return View();
+        }
+
+        // POST: Trip/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TripMiles,TripDate,TripRating,BikerInfoId")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(trip);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BikerInfoId"] = new SelectList(_context.BikerInfos, "Id", "Id", trip.BikerInfoId);
+            return View(trip);
+        }
+
+        // GET: Trip/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trip = await _context.Trips.FindAsync(id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+            ViewData["BikerInfoId"] = new SelectList(_context.BikerInfos, "Id", "Id", trip.BikerInfoId);
+            return View(trip);
+        }
+
+        // POST: Trip/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TripMiles,TripDate,TripRating,BikerInfoId")] Trip trip)
+        {
+            if (id != trip.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(trip);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TripExists(trip.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BikerInfoId"] = new SelectList(_context.BikerInfos, "Id", "Id", trip.BikerInfoId);
+            return View(trip);
+        }
+
+        // GET: Trip/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trip = await _context.Trips
+                .Include(t => t.BikerInfo)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return View(trip);
+        }
+
+        // POST: Trip/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var trip = await _context.Trips.FindAsync(id);
+            _context.Trips.Remove(trip);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TripExists(int id)
+        {
+            return _context.Trips.Any(e => e.Id == id);
+        }
+    }
+}
