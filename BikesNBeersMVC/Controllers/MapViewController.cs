1f4c059e48fad5b1c850ab2a58179ef7c21118ed
jason-aultman <jason.aultman@gmail.com>
2020-12-14 23:17:36 -0500

    map view fix

--- BikesNBeersMVC/Controllers/MapViewController.cs
+++ BikesNBeersMVC/Controllers/MapViewController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BikesNBeersMVC.Context;
 using BikesNBeersMVC.Models;
 using BikesNBeersMVC.Services;
 using BikesNBeersMVC.Services.Interfaces;
@@ -12,34 +13,21 @@ namespace BikesNBeersMVC.Controllers
     public class MapViewController : Controller
     {
         public readonly IStopHandler _stopHandler;
+        public readonly ApplicationDbContext _applicationDbContext;
 
-        public MapViewController(IStopHandler stopHandler)
+        public MapViewController(IStopHandler stopHandler, ApplicationDbContext dbContext)
         {
+            _applicationDbContext = dbContext;
             _stopHandler = stopHandler;
         }
-        public async Task<IActionResult> Index(Trip route)
+      //  [Route ("Mapview/Index/{tripId:int}")]
+        public IActionResult Index(int tripId)
         {
-            var testPlace1 = new Trip();
-            var testPlace2 = new Trip();
-            testPlace1.Id = 0;
-            testPlace1.BikerInfoId = 0;
-            testPlace1.Stops.Add(new Stop() { Id = 0, Address = "2611 William Ave, Holland, MI 49424" });
-            testPlace2.Id = 1;
-            testPlace2.BikerInfoId = 1;
-            testPlace2.Stops.Add(new Stop() { Id = 0, Address = "528 South Brooks Rd, Muskegon, MI 49442" });
-
-            //test
-            var place1 = testPlace1.Stops[0].Address;
-            var place2 = testPlace2.Stops[0].Address;
-            var mapViewModel = new MapViewModel();
-            var start = await _stopHandler.GetCoordinatesByAddress(place1);
-            var end = await _stopHandler.GetCoordinatesByAddress(place2);
-            mapViewModel.StartLat = start.results[0].geometry.location.lat;
-            mapViewModel.StartLng = start.results[0].geometry.location.lng;
-            mapViewModel.EndLat = end.results[0].geometry.location.lat;
-            mapViewModel.EndLng = end.results[0].geometry.location.lng;
-            //var route = _routeHandler.GetRoute(start, end);
-            return View(mapViewModel);
+            var stopList = new List<Stop>();
+            stopList = _applicationDbContext.Stops.Where(_ => _.TripId == tripId).ToList();
+            
+           
+            return View(stopList);
         }
     }
 }
